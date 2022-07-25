a91579b8610b7bd4f8316161b0201b0da677f15f
ramon <ramon@dedact.nl>
2022-07-25 08:44:06 -0500

    adds body parser to app and testsetup script

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ import cors from 'cors'
 import routes from './src/routes/setupRoutes.js'
 
 const port = process.env.port || 3000
-const app = express() 
+const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
 
 if (process.env.environment === 'dev' || !process.env.environment) {
-    app.use('*', (req, es, next) => {
-        console.log('current path: ', req.url)
-        next()
-    })
+	app.use('*', (req, es, next) => {
+		console.log('current path: ', req.url)
+		next()
+	})
 }
 
-routes.forEach((route) =>  {
-    app.use(route.default) // TODO remove wrapper around the default property.
+routes.forEach((route) => {
+	app.use(route.default) // TODO remove wrapper around the default property.
 })
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+	console.log(`App listening on port ${port}`)
 })
-
--- src/routes/__tests__/setup.js
+++ src/routes/__tests__/setup.js
@@ -1,13 +1,11 @@
-import express from "express"
+import express from 'express'
 import routes from '../setupRoutes'
 
 const app = express()
+app.use(bodyParser.json())
 
-routes.forEach((route) =>  {
-    app.use(route.default) // TODO remove wrapper around the default property.
+routes.forEach((route) => {
+	app.use(route.default) // TODO remove wrapper around the default property.
 })
 
 export default app
-
-
-
